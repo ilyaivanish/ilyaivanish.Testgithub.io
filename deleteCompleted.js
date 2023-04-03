f828805175f76b501b1ec6194aca93bd94177d2e
Ilya Ivanishin <ilyaivanish@gmail.com>
2023-04-03 12:30:28 +0300

    todoapp

--- deleteCompleted.js
+++
@@ -1,9 +0,0 @@
-const deleteCompletedBtn = document.querySelector('#deleteCompletedBtn');
-
-export const deleteCompleted = (tasksList, updateList) => {
-    deleteCompletedBtn.addEventListener('click', () => {
-        const filteredList = tasksList.filter(task => !task.completed);
-        tasksList = filteredList; 
-        updateList();
-    });
-};
